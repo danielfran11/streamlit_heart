338d104985990236ee297094975b5de348480136
danielfran11 <53270504+danielfran11@users.noreply.github.com>
2024-04-09 14:13:59 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,19 +9,32 @@ model = pickle.load(open('model.sav', 'rb'))
 # judul web
 st.title('Prediksi Penyakit Jantung')
 
-age = st.text_input ('Masukan usia (age)')
-sex = st.text_input (' Masukan jenis kelamin (sex)')
-cp = st.text_input ("Masukan tingkatan nyeri dada (cp)")
-trestbps = st.text_input ('Masukan angka tekanan darah (trestbps)')
-chol = st.text_input ('Masuka angka kolesterol (chol)')
-fbs = st.text_input ('Masukan angka gula darah puasa (fbs)')
-restecg = st.text_input ('Masukan angka resting electrocardiographic (restecg)')
-thalach = st.text_input ('Masukan angka detak jantung maksimum (thalach)')
-exang = st.text_input ('Masukan angka exercise induced angina (exang)')
-oldpeak = st.text_input ('Masukan angka depression induced (oldpeak)')
-slope = st.text_input ('Masukan angka kemiringan segmen latihan ST (slope)')
-ca = st.text_input ('Masukan angka major vessels (ca)')
-thal = st.text_input ('Masukan angka tipe jenis defek jantung (thal)')
+with col1:
+    age = st.text_input ('Masukan usia (age)')
+with col2:
+    sex = st.text_input (' Masukan jenis kelamin (sex)')
+with col1:
+    cp = st.text_input ("Masukan tingkatan nyeri dada (cp)")
+with col2:
+    trestbps = st.text_input ('Masukan angka tekanan darah (trestbps)')
+with col1:
+    chol = st.text_input ('Masukan angka kolesterol (chol)')
+with col2:
+    fbs = st.text_input ('Masukan angka gula darah puasa (fbs)')
+with col1:
+    restecg = st.text_input ('Masukan angka resting electrocardiographic (restecg)')
+with col2:
+    thalach = st.text_input ('Masukan angka detak jantung maksimum (thalach)')
+with col1:
+    exang = st.text_input ('Masukan angka exercise induced angina (exang)')
+with col2:
+    oldpeak = st.text_input ('Masukan angka depression induced (oldpeak)')
+with col1:
+    slope = st.text_input ('Masukan angka kemiringan segmen latihan ST (slope)')
+with col2:
+    ca = st.text_input ('Masukan angka major vessels (ca)')
+with col1:
+    thal = st.text_input ('Masukan angka tipe jenis defek jantung (thal)')
 
 
 heart_diagnose = ' '
